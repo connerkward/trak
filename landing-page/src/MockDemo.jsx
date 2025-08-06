26a9696071240eb85d75c2122b913e8e55672cda
CONNER WARD <conner.ward@mercedes-benz.com>
2025-08-06 02:08:58 -0700

    fix

--- landing-page/src/MockDemo.jsx
+++ landing-page/src/MockDemo.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import '../../src/App.css';
+import '../../src/renderer/main/App.css';
 
 const MockDemo = () => {
   const [timers, setTimers] = useState([
